95ff97a7552dd860ed75ae4ba73a828aeed97ca2
Aaronchat <pughenbohm@yahoo.com>
2026-08-01 21:57:56 -0700

    Delete prompt.js

--- prompt.js
+++
@@ -1,8 +0,0 @@
-let character,outfit,location,chaos;
-function renderPrompt(){
-characterEl.textContent=character;
-outfitEl.textContent=outfit;
-locationEl.textContent=location;
-chaosEl.textContent=chaos.text;
-prompt.value=`Photorealistic. Story orientation. ${character}. Wearing ${outfit}. At ${location}. ${chaos.text}.`;
-}
