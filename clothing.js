2f6bceb78de99e4f21e758bc443f60590c546fbd
Aaronchat <pughenbohm@yahoo.com>
2026-08-03 13:25:58 -0700

    Add files via upload

--- clothing.js
+++ clothing.js
@@ -1,1 +1,21 @@
-// Placeholder for future modularization
+export const clothingData = {
+
+Top:["🎲 Random","None","T-Shirt","Scoop-Neck Tee","V-Neck Tee","Henley","Long Sleeve Tee","Tank Top","Ribbed Tank Top","Crop Top","Cami Top","Halter Top","Wrap Top","Off-Shoulder Top","Bardot Top","Sweetheart Top","Peasant Blouse","Blouse","Button-Up Shirt","Tie-Front Shirt","Sports Bra","Bralette","Athletic Tank","Compression Top","Satin Blouse","Silk Blouse","Bodysuit","Corset Top","Knit Sweater","Fitted Turtleneck","Square-Neck Top","Ruched Top","Lace Blouse","Lace Cami","Racerback Tank","Knit Cami","Fitted Polo Shirt","Quarter-Zip Top","Baseball Tee","Ribbed Henley","Off-Shoulder Sweater","Mock Neck Top"],
+Bottom:["🎲 Random","None","Blue Jeans","Skinny Jeans","Straight Jeans","Bootcut Jeans","Leggings","Yoga Pants","Cargo Pants","Leather Pants","Denim Shorts","Athletic Shorts","Running Shorts","Biker Shorts","Khaki Shorts","Linen Shorts","Mini Skirt","Denim Skirt","Pencil Skirt","Pleated Skirt","Midi Skirt","Maxi Skirt","Cargo Shorts","High-Waisted Jeans","Boyfriend Jeans","Wide-Leg Jeans","Flare Jeans","Corduroy Pants","Utility Pants","A-Line Skirt","Wrap Skirt"],
+Dresses:["🎲 Random","None","Sundress","Mini Dress","Maxi Dress","Bodycon Dress","Evening Gown","Ball Gown"],
+BikiniTop:["🎲 Random","None","Triangle Bikini Top","String Bikini Top","Bandeau Bikini Top","Tube Bikini Top","Halter Bikini Top","Underwire Bikini Top","Sports Bikini Top","Tankini Top","High-Neck Bikini Top"],
+BikiniBottom:["🎲 Random","None","Classic Bikini Bottom","Brazilian Bikini Bottom","Thong Bikini Bottom","High-Waisted Bikini Bottom","Boyshort Bikini Bottom","Cheeky Bikini Bottom","Side-Tie Bikini Bottom"],
+OnePiece:["🎲 Random","None","One-Piece Swimsuit","Plunging One-Piece","High-Neck One-Piece","Cut-Out One-Piece","Monokini","Athletic One-Piece"],
+SpecialtySwimwear:["🎲 Random","None","Rash Guard","Rash Guard Set","Surf Suit","Dive Suit","Wetsuit","Springsuit"],
+CompleteOutfit:["🎲 Random","None","Jumpsuit","Coveralls","Romper","Pink Bunny Costume","Full-Body Pink Bunny Suit"],
+Theme:["🎲 Random","None","Fantasy","Sci-Fi","Historical","Occupations","Athletic","Costumes","Cultural"],
+ThemeOutfit:["🎲 Random","None"],
+Footwear:["🎲 Random","Sneakers","White Sneakers","Running Shoes","Skate Shoes","Ankle Boots","Chelsea Boots","Cowboy Boots","Combat Boots","Knee-High Boots","Ballet Flats","Loafers","Oxfords","Pumps","Stilettos","Sandals","Flip-Flops","Slides","Espadrilles","Hiking Boots","Trail Shoes"],
+Outerwear:["🎲 Random","None","Denim Jacket","Leather Jacket","Bomber Jacket","Blazer","Cardigan","Hoodie","Zip Hoodie","Windbreaker","Trench Coat","Peacoat","Parka","Moto Jacket","Cropped Denim Jacket","Cropped Leather Jacket","Tailored Blazer","Quilted Vest"],
+Accessories:accessoriesData,
+"Historical":["Roman Toga","Greek Chiton","Medieval Peasant Dress","Noblewoman Gown","Renaissance Dress","Regency Dress","Roaring 20s Flapper","Victorian Mourning Dress","Western Saloon Dress"],
+"Athletic":["Tennis Outfit","Golf Outfit","Volleyball Uniform","Soccer Kit","Basketball Uniform","Swimsuit","Running Outfit","Cycling Kit","Gym Wear"],
+"Occupations":["Doctor","Nurse","Firefighter","Police Officer","Mechanic","Construction Worker","Chef","Flight Attendant","Scientist","Astronaut","Detective","Librarian","Teacher","Barista"],
+"Costumes":["Full-Body Pink Bunny Suit","Playboy Bunny","Cat Costume","Fox Costume","Cowgirl","Cow Costume","Maid Outfit","School Uniform","Cheerleader","Devil Costume","Angel Costume","Vampire","Zombie","Superhero","Supervillain","Clown","Ringmaster","Magician","Mime","Jester"],
+"Cultural":["Kimono","Yukata","Hanbok","Qipao","Saree","Dirndl","Lederhosen","Scottish Kilt","Mexican Charro Outfit"]
+}
