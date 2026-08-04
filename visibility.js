e300e73b69c7b57cabf9129123de80461b2eff27
Aaronchat <pughenbohm@yahoo.com>
2026-08-03 20:06:32 -0700

    Add files via upload

---
+++ visibility.js
@@ -0,0 +1,28 @@
+export const visibilityRegions = [
+  "Head",
+  "Face",
+  "Neck",
+
+  "Chest",
+  "Abdomen",
+
+  "Left Upper Arm",
+  "Left Lower Arm",
+  "Left Wrist",
+  "Left Hand",
+
+  "Right Upper Arm",
+  "Right Lower Arm",
+  "Right Wrist",
+  "Right Hand",
+
+  "Left Upper Leg",
+  "Left Lower Leg",
+  "Left Ankle",
+  "Left Foot",
+
+  "Right Upper Leg",
+  "Right Lower Leg",
+  "Right Ankle",
+  "Right Foot"
+];
